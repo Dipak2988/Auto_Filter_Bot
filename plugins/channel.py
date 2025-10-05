41d8022ceac7df6a1fd0cb0fa9a645a8077c59b2
Dipak2988 <dipaksinghasarkar@gmail.com>
2025-10-05 14:37:47 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -319,8 +319,8 @@ async def send_movie_update(bot, base_name):
             text = generate_movie_message(movie_doc, base_name)
             buttons = InlineKeyboardMarkup([[
                 InlineKeyboardButton(
-                    'ɢᴇᴛ ғɪʟᴇs',
-                    url=f"https://t.me/{temp.U_NAME}?start=getfile-{base_name.replace(' ', '-')}"
+                    'ꜱᴇᴀʀᴄʜ ɪɴ ɢʀᴏᴜᴘ',
+                    url=f"https://t.me/+fVnl-lXy6wNmM2Fl"
                 )
             ]])
 
@@ -368,8 +368,8 @@ async def update_movie_message(bot, base_name):
         text = generate_movie_message(movie_doc, base_name)
         buttons = InlineKeyboardMarkup([[
             InlineKeyboardButton(
-                'ɢᴇᴛ ғɪʟᴇs',
-                url=f"https://t.me/{temp.U_NAME}?start=getfile-{base_name.replace(' ', '-')}"
+                'ꜱᴇᴀʀᴄʜ ɪɴ ɢʀᴏᴜᴘ',
+                url=f"https://t.me/+fVnl-lXy6wNmM2Fl"
             )
         ]])
 
