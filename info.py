02f3524b19c8614c66b29645489b1af2f13680a4
Dipak2988 <dipaksinghasarkar@gmail.com>
2025-10-17 17:21:02 +0530

    Update VERIFY_IMG URL to a new image link

--- info.py
+++ info.py
@@ -98,7 +98,7 @@ LANDSCAPE_POSTER = bool(environ.get('LANDSCAPE_POSTER', True)) # Shows landscape
 IS_VERIFY = is_enabled('IS_VERIFY', True)  # Verification On (True) / Off (False)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002168852711')) #Verification Channel Id 
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002168852711')) #If Anyone Set Your Bot In Any Group And Set Shortner In That Group Then In This Channel The All Details Come
-VERIFY_IMG = environ.get("VERIFY_IMG", "https://envs.sh/YtO.jpg")
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://te.legra.ph/file/ef1384fc22d22c26163a8-cddd7d553f335c5cce.png")
 
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/HowToDownloadMovie2023/10")   # Tutorial link for verification
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/HowToDownloadMovie2023/8")   # Second tutorial link for verification
