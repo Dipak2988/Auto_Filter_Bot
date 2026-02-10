aaf632c01e3f16b4c6c4bc1ab9ae095bb4845753
Dipak2988 <dipaksinghasarkar@gmail.com>
2026-02-11 00:21:49 +0530

    Change verification gap defaults to 60 seconds
    Updated the default values for TWO_VERIFY_GAP and THREE_VERIFY_GAP to 60 seconds.

--- info.py
+++ info.py
@@ -114,8 +114,8 @@ SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "arolinks.com") # Shorten
 SHORTENER_API3 = environ.get("SHORTENER_API3", "00f526aae924309d5f31961906b46df8d3310bc3")  
 SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "bongurl.in") # Shortener website for third website
 
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "300")) # Time gap for two-step verification in seconds (default: 20 minutes)
-THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "600"))    
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "60")) # Time gap for two-step verification in seconds (default: 20 minutes)
+THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "60"))    
 
 # ============================
 # Channel & Group Links Configuration
