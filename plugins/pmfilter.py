a7f5e3f10e6d8259ef503034ed3741361eb55f8e
Dipak2988 <dipaksinghasarkar@gmail.com>
2026-09-23 15:37:06 +0530

    Update button URLs in pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1294,7 +1294,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('ᴅʀᴇᴀᴍxʙᴏᴛᴢ 📜', url='https://github.com/DreamXBotz/Auto_Filter_Bot.git'),
+            InlineKeyboardButton('OWNER 📜', url='https://t.me/dipaksinghasarkar'),
             InlineKeyboardButton('⇋ ʙᴀᴄᴋ ⇋', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
